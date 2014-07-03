ba61d9860b7e0c7b60b0275e5074f191e8bb4871
Nathaniel Biggs <biggs0125@gmail.com>
2014-07-03 01:58:07 -0400

    corrected page/urls.py

--- Anne/page/urls.py
+++ Anne/page/urls.py
@@ -4,7 +4,7 @@ from page import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
-    url(r'^photography/' views.photo, name='photo'),
+    url(r'^photography/', views.photo, name='photo'),
     url(r'^art/', views.art, name='art'),
     url(r'^design/', views.design, name='design'),
     url(r'^contact/', views.contact, name='contact'),
