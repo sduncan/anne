876dfce864cba8915f49c906a39766725875da09
Nathaniel Biggs <biggs0125@gmail.com>
2014-07-03 01:07:12 -0400

    altered path to template

--- Anne/page/views.py
+++ Anne/page/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from Anne import settings
 
 def home(request):
-    return render('home.html')
+    return render(request,settings.BASE_DIR+'/page/templates/home.html')
