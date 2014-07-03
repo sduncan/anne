d06cb41ee2062245a771fe870e1d51e74dd75b9b
Nathaniel Biggs <biggs0125@gmail.com>
2014-07-03 01:33:59 -0400

    Added rest of urls and views

--- Anne/page/views.py
+++ Anne/page/views.py
@@ -4,4 +4,22 @@ from django.shortcuts import render
 from Anne import settings
 
 def home(request):
-    return render(request,settings.BASE_DIR+'/page/templates/home.html')
+    return render(request,'home.html')
+
+def photo(request):
+    return render(request,'photo.html')
+
+def art(request):
+    return render(request,'art.html')
+
+def design(request):
+    return render(request,'design.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def anneadminlogin(request):
+    return render(request,'login.html')
+
+def anneadmin(request):
+    return render(request,'admin.html')
